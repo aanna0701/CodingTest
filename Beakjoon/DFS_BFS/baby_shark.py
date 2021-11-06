fd317ecc6b62bbccf2f535045a6e5324acf3b58c
Lee SeungHoon <aanna0701@gmail.com>
2021-11-06 23:30:43 +0900

    update

--- Beakjoon/DFS_BFS/baby_shark.py
+++ Beakjoon/DFS_BFS/baby_shark.py
@@ -27,31 +27,43 @@ def bfs(x, y):
     
     q = deque()
     q.append((x, y))
+    
+    # 2차원 방문 배열
     visit = [[0]*space_size for _ in range(space_size)]
     visit[x][y] = 1
     
+    # 2차원 거리 배열
     distance = [[0]*space_size for _ in range(space_size)]
+    
     eat = list()
+    
+    # 현재 아기상어 크기
     current_size = size[x][y]
     
     while q:
         x, y = q.popleft()
         
+        # 네 방향 탐색
         for i in range(4):
             m_x = x + dx[i]
             m_y = y + dy[i]
             
+            # 지도를 넘어간 경우 못지남
             if m_x < 0 or m_x > space_size-1 or m_y < 0 or m_y > space_size-1:
                 continue
             
+            # 아기 상어보다 큰 물고기와 이미 탐색한 경우 못지남
             if size[m_x][m_y] > current_size or visit[m_x][m_y] == 1:
                 continue    
             
+            # 조건 만족하면 탐색
             q.append((m_x, m_y))
-            distance[m_x][m_y] = distance[x][y] + 1
-            visit[m_x][m_y] = 1
+            distance[m_x][m_y] = distance[x][y] + 1 # 기준점으로 부터 거리를 거리 배열에 표시
+            visit[m_x][m_y] = 1                     # 방문 기록
             
+            # 아기 상어 보다 크기가 작을경우 먹음
             if size[m_x][m_y] > 0 and size[m_x][m_y] < current_size:
+                # 아기 상어로 부터 거리와 위치를 배열로 표시
                 eat.append((distance[m_x][m_y], m_x, m_y))
         
         # 0, 2, 1 우선 순위로 정렬
@@ -65,18 +77,22 @@ while(True):
     
     eat = bfs(x, y)
     
+    # 종료 조건: 더 이상 물고기가 없을 경우
     if not eat:
         print(time)
         break
     
     eat = eat[0]
+    # 물고기를 먹는데 걸린 시간
     time += eat[0]
+    # 경험치 표시
     cnt += 1
+    # 물고기 위치 업데이트
     size[eat[1]][eat[2]] = size[x][y]
     size[x][y] = 0
-    
     x, y = eat[1], eat[2]
     
+    # 경험치가 차면 아기 상어 크기 증가
     if cnt == size[eat[1]][eat[2]]:
         size[eat[1]][eat[2]] += 1
         cnt = 0
